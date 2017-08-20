1dbd3cf37e781c0c6bab08505571a3b6785ec273
Piotr Zduniak <piotr@zduniak.net>
2017-08-20 21:11:13 +0200

    libbrotlicommon out

--- go/cbrotli/cgo.go
+++ go/cbrotli/cgo.go
@@ -7,7 +7,6 @@ package cbrotli
 
 // Inform golang build system that it should link brotli libraries.
 
-// #cgo LDFLAGS: -lbrotlicommon
 // #cgo LDFLAGS: -lbrotlidec
 // #cgo LDFLAGS: -lbrotlienc
 import "C"
